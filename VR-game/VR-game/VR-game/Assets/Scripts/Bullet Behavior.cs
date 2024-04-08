20e9034710228d2a798f96f2aa415207c8f96ff0
sverhdimon07 <86204314+sverhdimon07@users.noreply.github.com>
2024-04-09 01:17:38 +0300

    commit 8
    Все готово для написания 9го отчета.
    + 2 хелс бара
    + механика стрельбы

---
+++ VR-game/VR-game/VR-game/Assets/Scripts/Bullet Behavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BulletBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float BulletSpawnDelay;
+    private float newTimeBulletSpawn = 0.0f;
+
+    [SerializeField] private InputActionProperty gripClick;
+    [SerializeField] private InputActionProperty triggerClick;
+    private void Update()
+    {
+        if ((gripClick.action.ReadValue<float>() > 0) && (triggerClick.action.ReadValue<float>() > 0))
+        {
+            BulletCreation();
+        }
+    }
+    private void BulletCreation()
+    {
+        if (Time.time < newTimeBulletSpawn)
+        {
+            return;
+        }
+        newTimeBulletSpawn = Time.time + BulletSpawnDelay;
+        GameObject bulletCopy = Instantiate(obj, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Destroy(bulletCopy, 3.0f);
+    }
+}
